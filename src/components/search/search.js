497e76f635d90a44918bf79c853fedf8c133ec3a
nergız seyidova <nergisseyidova@gmail.com>
2023-04-24 00:44:10 +0400

    search

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,11 +1,18 @@
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useState } from 'react';
 import { ContextTodos } from '@/pages/contextTodos';
 
 export default function Searchbox() {
   const [todosreducer, dispatch] = useContext(ContextTodos);
   const searchinpt = useRef();
+  const [searchlist, setSearchList] = useState([]);
 
   function search() {
+    const value = searchinpt.current.value;
+    todosreducer.map((todo) => {
+      if (value == todo.name) {
+        setSearchList([...searchlist, todo.name]);
+      }
+    });
     return;
   }
   return (
@@ -13,6 +20,7 @@ export default function Searchbox() {
       <p>search for</p>
       <input type="text" ref={searchinpt}></input>
       <button onClick={search}>search</button>
+      <div>{searchlist.map((item) => item)}</div>
     </div>
   );
 }
